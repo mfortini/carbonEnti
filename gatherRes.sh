a69fde0665987e8302f78acf2d71ef7f9ed9d666
Matteo Fortini <matteo.fortini@gmail.com>
2022-01-11 17:37:34 +0100

    Add header

--- gatherRes.sh
+++ gatherRes.sh
@@ -1,1 +1,2 @@
-find entiRes -name \*.json -exec jq -r '[.Codice_IPA,.url,.lighthouseScore,."first-meaningful-paint".numericValue,."total-byte-weight".numericValue] | @csv' '{}' \; | tee entiRes.csv
+echo "Codice_IPA,url,lighthouseScore,firstMeaningfulPaint,totalByteWeight" > entiRes.csv
+find entiRes -name \*.json -exec jq -r '[.Codice_IPA,.url,.lighthouseScore,."first-meaningful-paint".numericValue,."total-byte-weight".numericValue] | @csv' '{}' \; | tee -a entiRes.csv
