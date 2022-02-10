85a5cfb2eac817f611e0c1927ac9a0af1015df88
Matteo Fortini <matteo.fortini@gmail.com>
2022-02-10 11:41:29 +0100

    Add bootstrap columns

--- gatherRes.sh
+++ gatherRes.sh
@@ -1,2 +1,2 @@
-echo "Codice_IPA,url,lighthouseScore,firstMeaningfulPaint,totalByteWeight" > entiRes.csv
-find entiRes -name \*.json -exec jq -r '[.Codice_IPA,.url,.lighthouseScore,."first-meaningful-paint".numericValue,."total-byte-weight".numericValue] | @csv' '{}' \; | tee -a entiRes.csv
+echo "Codice_IPA,url,lighthouseScore,firstMeaningfulPaint,totalByteWeight,bootstrap,bootstrapItalia" > entiRes.csv
+find entiRes -name \*.json -exec jq -r '[.Codice_IPA,.url,.lighthouseScore,."first-meaningful-paint".numericValue,."total-byte-weight".numericValue,.bootstrapItalia.bootstrap,.bootstrapItalia.bootstrapItalia] | @csv' '{}' \; | tee -a entiRes.csv
