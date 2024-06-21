3ed327e419837f2d29051c30c62cebcf76b4e864
Matteo Fortini <matteo.fortini@gmail.com>
2024-06-21 12:31:31 +0200

    Aggiunta gather dati bootstrap2

--- gatherRes.sh
+++ gatherRes.sh
@@ -1,2 +1,2 @@
-echo "Codice_IPA,url,lighthouseScore,firstMeaningfulPaint,totalByteWeight,bootstrap,bootstrapItalia" > entiRes.csv
-find entiRes -name \*.json -exec jq -r '[.Codice_IPA,.url,.lighthouseScore,."first-meaningful-paint".numericValue,."total-byte-weight".numericValue,.bootstrapItalia.bootstrap,.bootstrapItalia.bootstrapItalia] | @csv' '{}' \; | tee -a entiRes.csv
+echo "Codice_IPA,url,lighthouseScore,firstMeaningfulPaint,totalByteWeight,bootstrap,bootstrapItalia,bootstrap2_js,bootstrap2_css" > entiRes.csv
+find entiRes -name \*.json -exec jq -r '[.Codice_IPA,.url,.lighthouseScore,."first-meaningful-paint".numericValue,."total-byte-weight".numericValue,.bootstrapItalia.bootstrap,.bootstrapItalia.bootstrapItalia,.bootstrapItalia2.bootstrapItaliaVariable,.bootstrapItalia2.bootstrapItaliaMethod] | @csv' '{}' \; | tee -a entiRes.csv
