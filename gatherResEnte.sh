a7f3c77fa07c2fb9901bdf6313ddf988b3b2647d
Matteo Fortini <matteo.fortini@gmail.com>
2024-08-06 09:49:10 +0200

    Fix gatherResEnte

--- gatherResEnte.sh
+++ gatherResEnte.sh
@@ -4,6 +4,6 @@ filename=$1
 
 Codice_IPA=$(basename "${filename}" .json)
 
-line=$(jq -r '[.url,.lighthouseScore,."first-meaningful-paint".numericValue,."total-byte-weight".numericValue,.bootstrapItalia.bootstrap,.bootstrapItalia.bootstrapItalia,.bootstrapItalia.bootstrapItaliaVariable,.bootstrapItalia.bootstrapItaliaMethod] | @csv' "${filename}")
+line=$(jq -r '[.url,.lighthouseScore, .rawResult.audits."largest-contentful-paint".numericValue,."total-byte-weight".numericValue,.bootstrapItalia.bootstrap,.bootstrapItalia.bootstrapItalia,.bootstrapItalia.bootstrapItaliaVariable,.bootstrapItalia.bootstrapItaliaMethod] | @csv' "${filename}")
 
 echo ${Codice_IPA},${line}
